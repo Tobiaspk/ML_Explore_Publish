bd047f8a842e93d7139188d7e3c67dd46b5be3c6
Tobias Krause <tobiaspk1@e247-150.eduroam.tuwien.ac.at>
2019-11-14 15:15:14 +0100

    Updated model definitions

--- Exercise1/DefineModels.py
+++ Exercise1/DefineModels.py
@@ -3,8 +3,7 @@ from DataSetting import *
 from Regressor import * 
 from HelpFunctions import *
 
-from sklearn.linear_model import LinearRegression, TheilSenRegressor, RANSACRegressor, HuberRegressor
-from sklearn.linear_model import RidgeCV, LassoCV, BayesianRidge
+from sklearn.linear_model import LinearRegression, TheilSenRegressor, RANSACRegressor, HuberRegressor, Ridge, Lasso
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.neighbors import KNeighborsRegressor
@@ -14,66 +13,60 @@ random_state = 123
 
 ## SET PARAMETERS
 # Params KNN Regressor
-n_neighbors_temp = [(3*i) for i in range(1,6)]
+n_neighbors_temp = [2, 4, 8, 12]
 weights_temp = ['uniform', 'distance']
-algorithm_temp = ['ball_tree', 'kd_tree', 'brute', 'auto']
-params_knn = [{'n_neighbors': i, 'weights': j, 'algorithm': k} for i in n_neighbors_temp for j in weights_temp for k in algorithm_temp]
+params_knn = [{'n_neighbors': i, 'weights': j} for i in n_neighbors_temp for j in weights_temp]
+
+# Params Huber
+params_huber = [{'epsilon:':i} for i in np.linspace(1, 2, 6)]
 
 # Params Ridge
-params_ridge = [{"cv":5}]
+params_ridge = [{'alpha:':i} for i in [.1, .9, 1, 2, 4]]
 
 # Params Lasso
-params_lasso = [{"cv":5}]
+params_lasso = [{'alpha:':i} for i in [.1, .9, 1, 2, 4]]
 
 # Params Decistion Tree
 max_depth_temp = [(i+1)*3 for i in range(5)]
-max_leaf_nodes_temp = [None] + [(i+1)*3 for i in range(4)]
-min_samples_splits_temp = np.linspace(0.1, 1.0, 5)
-params_dt = [{'max_depth':i, 'max_leaf_nodes':j, 'min_samples_split':k} for i in max_depth_temp for j in max_leaf_nodes_temp for k in min_samples_splits_temp]
+min_samples_split_temp = [(i+1)*3 for i in range(3)]
+max_features_temp = ["sqrt", "log2", "None"]
+params_dt = [{'max_depth':i, 'min_samples_split':j, 'max_features':k} for i in max_depth_temp for j in min_samples_split_temp for k in max_features_temp]
 
 # Params Random Forest
-n_estimators_temp = np.linspace(100, 1000, 2, dtype= np.dtype(np.int16))  #10
-max_depth_temp = np.linspace(10, 100, 2) #10
-min_samples_split_temp = [2,5]# [2, 5, 8, 11]
-min_samples_leaf_temp = [1]# [1, 3, 5]
-#params_rf = [{"n_estimators":i, 'max_depth':j, 'min_samples_split':k, 'min_samples_leaf':l} for i in n_estimators_temp for j in max_depth_temp for k in min_samples_split_temp for l in min_samples_leaf_temp]
-params_rf = [{"n_estimators": 100, 'max_depth': 10, 'min_samples_split': 2, 'min_samples_leaf':1}]
+n_estimators_temp = [10, 100, 1000]
+max_depth_temp = [10, 100]
+min_samples_split_temp = [2,5]
+params_rf = [{"n_estimators": i, 'max_depth': j, 'min_samples_split': k} for i in n_estimators_temp for j in max_depth_temp, for k in min_sampls_split_temp]
 
 # Params XGBOOST
-eta_temp = np.linspace(0.01, 0.3, 5) #10
-max_depth_temp = np.linspace(3, 8, 4, dtype= np.dtype(np.int16)) #8
-gamma_temp = np.linspace(0, 0.2, 5) #10
-subsample_temp = np.linspace(0.5, 1, 3) #6
-colsample_bytree_temp = np.linspace(0.5, 1, 3) #6
-alpha_temp = np.linspace(0, 0.1, 5) #11
-min_child_weight_temp = np.linspace(1, 20, 5) #10
+eta_temp = [.01, .1, .3]
+gamma_temp = [0, .2, 1]
+lambda_temp = [1, 1.5, 5]
 
-params_xgb = [{'objective':  'reg:squarederror','eta':i, 'max_depth':j, 'gamma':k, 'subsample':l, 'colsample_bytree':m, 'alpha':n, 'min_child_weight':o} for i in eta_temp for j in max_depth_temp for k in gamma_temp for l in subsample_temp for m in colsample_bytree_temp for n in alpha_temp for o in min_child_weight_temp]
-#params_xgb = [{'eta':0.02, 'max_depth':3, 'gamma':0, 'subsample':0.5, 'colsample_bytree':0.5, 'alpha':0, 'min_child_weight': 1}]
+params_xgb = [{"eta": i, "gamma": j, "lambda": k} for i in eta_temp for j in gamma_temp for k in lambda_temp]
+
+
+########### MODELS
 models = []
 
 ## ADD MODELS
 # add linear models
 models += [Regressor("OLS", LinearRegression, [{}])]
-models += [Regressor("ThSen", TheilSenRegressor, [{'random_state':random_state}])]
-#models += [Regressor("Ransac", RANSACRegressor, [{'random_state':random_state}])]
-models += [Regressor("Huber", HuberRegressor, [{}])]
+models += [Regressor("ThSen", TheilSenRegressor, [{}])]
+models += [Regressor("Huber", HuberRegressor, params_huber)]
 
 # ridge and lasso
 #models += [Regressor("Ridge", RidgeCV, params_ridge)]
 models += [Regressor("Lasso", LassoCV, params_lasso)]
 
-# Bayesian Ridge
-models += [Regressor("BayRidge", BayesianRidge, [{}])]
-
 # KNN Regressor
-#models += [Regressor("KNN", KNeighborsRegressor, params_knn)]
+models += [Regressor("KNN", KNeighborsRegressor, params_knn)]
 
 # add tree
-#models += [Regressor("DecTree", DecisionTreeRegressor, params_dt)]
+models += [Regressor("DecTree", DecisionTreeRegressor, params_dt)]
 
 # forest
-#models += [Regressor("Forest", RandomForestRegressor, params_rf)]
+models += [Regressor("Forest", RandomForestRegressor, params_rf)]
 
 # xgboost
-#models += [Regressor("XGBoost", xgb.XGBRegressor, params_xgb)]
+models += [Regressor("XGBoost", xgb.XGBRegressor, params_xgb)]
